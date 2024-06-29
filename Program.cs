0173c5c6e0aad15ca2d6f6991864126e9c38c96c
Hemavathi987 <136674928+Hemavathi987@users.noreply.github.com>
2024-06-29 13:50:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ifElse
+{
+    class demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any value");
+            int a=Convert.ToInt32(Console.ReadLine());
+            if (a==4)
+                Console.WriteLine("Its true");
+            else
+                Console.WriteLine("False");
+
+        }
+    }
+}
+
+
